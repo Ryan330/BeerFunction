8110a008094e168e1be26da35c3c54d3b355488e
Ryan Miller <38670174+Ryan330@users.noreply.github.com>
2018-05-01 09:39:31 -0400

    Add files via upload

--- beerfunction.py
+++ beerfunction.py
@@ -1,6 +1,8 @@
+#Song Lyrics
 def song_line(numbottles):
     return "%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around, %d bottles of beer on the wall!" % (numbottles, numbottles, numbottles - 1)
 
+#Bottle Countdown
 totalofbottles = 99
-for num in range(totalofbottles + 1, 0, -1):
+for num in range(totalofbottles, 0, -1):
     print(song_line(num))
